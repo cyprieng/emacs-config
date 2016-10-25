d4d856e0c8b8dcc96d7caaba722f2672af6b3a94
cyprieng <cyprien.guillemot@gmail.com>
2016-10-25 17:41:09 +0200

    Add evil

--- init.el
+++ init.el
@@ -6,6 +6,9 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
 
+(require 'evil)
+(evil-mode 1)
+
 (shell)
 (sr-speedbar-open)
 
@@ -27,7 +30,7 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(package-selected-packages (quote (spacemacs-theme jedi solarized-theme))))
+ '(package-selected-packages (quote (evil spacemacs-theme jedi solarized-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
