d05fc85da2f0dc07b26e7fdbd9f1063d5f4f9d76
cyprieng <cyprien.guillemot@gmail.com>
2016-10-26 12:30:40 +0200

    Add some packages

--- init.el
+++ init.el
@@ -18,8 +18,25 @@
 (desktop-save-mode 1)
 (load-theme 'spacemacs-dark t)
 
+(setq redisplay-dont-pause t
+  scroll-margin 1
+  scroll-step 1
+  scroll-conservatively 10000
+  scroll-preserve-screen-position 1)
+
+(elscreen-start)
+
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
+
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this-word)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this-word)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this-word)
+
+
 (global-set-key (kbd "C-c g") 'jedi:goto-definition)
 
 (custom-set-variables
@@ -30,7 +47,9 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(package-selected-packages (quote (evil spacemacs-theme jedi solarized-theme))))
+ '(package-selected-packages
+   (quote
+    (helm magit psvn diff-hl elscreen multiple-cursors evil spacemacs-theme jedi solarized-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
