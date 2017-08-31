961bb84fa37c953a571a53ab50ee1061be8d5b1a
Cyprien Guillemot <cyprien@octipas.com>
2017-08-31 09:47:29 +0200

    All file in same group tabbar

--- init.el
+++ init.el
@@ -50,6 +50,16 @@
 (require 'tabbar)
 (tabbar-mode t)
 
+(defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
+   "Returns the name of the tab group names the current buffer belongs to.
+ There are two groups: Emacs buffers (those whose name starts with '*', plus
+ dired buffers), and the rest.  This works at least with Emacs v24.2 using
+ tabbar.el v1.7."
+   (list (cond ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+               ((eq major-mode 'dired-mode) "emacs")
+               (t "user"))))
+ (setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
+
 ;; BASIC CONFIG & THEME
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
