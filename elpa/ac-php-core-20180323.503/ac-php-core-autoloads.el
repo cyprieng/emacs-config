fd8e25eb150bb2778dfe478a993d26ba874d4fd7
Cyprien Guillemot <cyprien@octipas.com>
2018-04-05 17:35:58 +0200

    Update packages; Change theme; Improve switch to buffer method

---
+++ elpa/ac-php-core-20180323.503/ac-php-core-autoloads.el
@@ -0,0 +1,27 @@
+;;; ac-php-core-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ac-php-core" "ac-php-core.el" (23238 5577
+;;;;;;  270700 511000))
+;;; Generated autoloads from ac-php-core.el
+
+(autoload 'ac-php-core-eldoc-setup "ac-php-core" "\
+Set up eldoc function and enable eldoc-mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-php-comm-tags-data.el" "ac-php-core-pkg.el")
+;;;;;;  (23238 5577 273491 539000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-php-core-autoloads.el ends here
--- init.el
+++ init.el
@@ -205,6 +205,57 @@
 ;; TABBAR
 (require 'tabbar)
 (tabbar-mode t)
+(set-face-attribute
+ 'tabbar-unselected nil
+ :background "gray30"
+ :foreground "white"
+ :box '(:line-width 5 :color "gray30" :style nil))
+(defun make-xpm-bar (color height width)
+  "Create an XPM bitmap of a bar."
+  (propertize
+   " " 'display
+   (let ((data (make-list height (make-list width 1)))
+         (color (or color "None")))
+     (create-image
+      (concat
+       (format "/* XPM */\nstatic char * percent[] = {\n\"%i %i 2 1\",\n\". c %s\",\n\"  c %s\","
+               (length (car data))
+               (length data)
+               color
+               color)
+       (apply #'concat
+              (cl-loop with idx = 0
+                       with len = (length data)
+                       for dl in data
+                       do (cl-incf idx)
+                       collect
+                       (concat "\""
+                               (cl-loop for d in dl
+                                        if (= d 0) collect (string-to-char " ")
+                                        else collect (string-to-char "."))
+                               (if (eq idx len) "\"};" "\",\n")))))
+      'xpm t :ascent 'center))))
+
+(defun my-tabbar-display-tab (tab)
+    (let ((label (if tabbar--buffer-show-groups
+                     (format "[%s]" (tabbar-tab-tabset tab))
+                   (format "%s" (tabbar-tab-value tab))))
+          (bar-color "#51afef")
+          (bar-height 25)
+          (bar-width 3)
+          (selected-p (eq tab (tabbar-selected-tab (tabbar-current-tabset)))))
+      (concat (when (and (display-graphic-p) selected-p)
+                (make-xpm-bar bar-color bar-height bar-width))
+              " "
+              (if tabbar-auto-scroll-flag
+                  label
+                (tabbar-shorten
+                 label (max 1 (/ (window-width)
+                                 (length (tabbar-view
+                                          (tabbar-current-tabset)))))))
+              " ")))
+
+(setq tabbar-tab-label-function #'my-tabbar-display-tab)
 
 (defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
    "Returns the name of the tab group names the current buffer belongs to.
@@ -221,7 +272,17 @@
 (scroll-bar-mode -1)
 (global-linum-mode t)
 (desktop-save-mode 1)
-(load-theme 'dracula t)
+
+(require 'doom-themes)
+
+;; THEME
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italic t)
+(load-theme 'doom-one t)
+(doom-themes-visual-bell-config)
+(doom-themes-neotree-config)
+(doom-themes-org-config)
+(add-to-list 'default-frame-alist '(background-color . "#282C34"))
 
 ;; SCROLL OPTION
 (setq redisplay-dont-pause t
@@ -328,7 +389,7 @@ buffer in current window."
 ;; SWITCH TO BUFFER -> EXCLUDE INTERNAL BUFFERS
 (defun my-switch-to-buffer ()
   (interactive)
-  (let ((completion-regexp-list '("\\`[^*]"
+  (let ((completion-regexp-list '("\\`\\([^*]\\|\*eshell\*\\|\*scratch\*\\)"
                                   "\\`\\([^T]\\|T\\($\\|[^A]\\|A\\($\\|[^G]\\|G\\($\\|[^S]\\|S.\\)\\)\\)\\).*")))
     (call-interactively 'switch-to-buffer)))
 (global-set-key [C-tab] 'my-switch-to-buffer)
@@ -394,7 +455,7 @@ buffer in current window."
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(package-selected-packages
    (quote
-    (term+ multi-term origami highlight-indent-guides all-the-icons-dired projectile doom-themes dracula-theme treemacs vimish-fold jedi vue-mode dtrt-indent neotree hungry-delete ac-php smartparens ace-window tabbar flycheck virtualenvwrapper helm magit psvn diff-hl elscreen multiple-cursors evil spacemacs-theme solarized-theme)))
+    (term+ multi-term origami highlight-indent-guides all-the-icons-dired projectile doom-themes dracula-theme treemacs vimish-fold jedi vue-mode dtrt-indent neotree hungry-delete ac-php smartparens ace-window tabbar flycheck virtualenvwrapper helm magit psvn diff-hl multiple-cursors evil spacemacs-theme solarized-theme)))
  '(safe-local-variable-values (quote ((project-venv-name . "pikuli")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
