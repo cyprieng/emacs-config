77c2c3da1cdf91c6d66e3c2447c86394fef45009
Cyprien Guillemot <cyprien@octipas.com>
2018-07-27 10:54:00 +0200

    Update packages; Add magit

---
+++ elpa/pfuture-20180715.524/pfuture-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pfuture" "20180715.524" "a simple wrapper around asynchronous processes" '((emacs "25.2")) :commit "e39d0d2953a5db7d9f567596865239012e506ac4" :url "https://github.com/Alexander-Miller/pfuture")
